67d2e1b8a03a9669d7c3f74eccc1aa6ac67074c5
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2024-03-25 00:07:35 +0500

    Add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatService } from './cats/cats.service';
+import { logger } from './logger/logger.middleware';
+import * as cors from 'cors';
+import helmet from 'helmet';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
   providers: [AppService, CatService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cors(), helmet(), logger)
+      .exclude({ path: '/cats', method: RequestMethod.GET })
+      .forRoutes(CatsController);
+  }
+}
